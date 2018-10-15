405f95ee50ef191fe900f5f95a7fcdc3ffd15491
Shadab Azam <shadab.aazam@berylsystems.com>
2018-10-15 12:56:53 +0530

    init

---
+++ app/src/main/java/app_lock/project/beryl/com/seatbooking/EdgeItem.java
@@ -0,0 +1,16 @@
+package app_lock.project.beryl.com.seatbooking;
+
+public class EdgeItem extends AbstractItem {
+
+    public EdgeItem(String label) {
+        super(label);
+    }
+
+
+
+    @Override
+    public int getType() {
+        return TYPE_EDGE;
+    }
+
+}
---
+++ app/src/main/java/app_lock/project/beryl/com/seatbooking/OnSeatSelected.java
@@ -0,0 +1,9 @@
+package app_lock.project.beryl.com.seatbooking;
+
+/**
+ * Created by chandrasekar on 14/02/16.
+ */
+public interface OnSeatSelected {
+
+    void onSeatSelected(int count);
+}
